09d416f8ade9cd7259f300dad16485f14fe46e05
przemek <pwenderholm@gmail.com>
2023-02-18 19:04:03 +0100

    Initial commit

---
+++ src/main/java/com/example/postmethodtexttable/PostMethodTextTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmethodtexttable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMethodTextTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMethodTextTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postmethodtexttable/PostMethodTextTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmethodtexttable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMethodTextTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
